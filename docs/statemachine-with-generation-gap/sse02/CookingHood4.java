0c656d5b927f4bd6bdf048ba74c6cd551c8ae621
Ulrik Pagh Schultz <ups@mmmi.sdu.dk>
2012-02-29 21:26:32 +0100

    extended with generation gap (external functions)

---
+++ docs/statemachine-with-generation-gap/sse02/CookingHood4.java
@@ -0,0 +1,78 @@
+/*
+Copyright (c) 2012, Ulrik Pagh Schultz, University of Southern Denmark
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met: 
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies, 
+either expressed or implied, of the University of Southern Denmark.
+*/
+
+package sse02;
+
+import javax.swing.JLabel;
+
+import sse02.SuperCookingHood.Requires;
+import statemachine.year1.library.GraphicalMachine;
+import statemachine.year1.cookinghood.CookingHood1.ControlGUI;
+
+public class CookingHood4 extends GraphicalMachine {
+	
+    public static class Operations implements Requires {
+
+		@Override
+		public void setMotorPower(int p1) {
+			System.out.println("Motor power set to "+p1);
+
+		}
+
+		@Override
+		public void setMaxPower() {
+			System.out.println("Motor power set to MAX");
+
+		}
+
+		@Override
+		public void powerOff() {
+			System.out.println("Motor power off");
+		}
+
+	}
+
+	public static void main(String argv[]) {
+        new CookingHood4();
+    }
+    
+    public CookingHood4() {
+        super(new ControlGUI(),new SuperCookingHood(new Operations()),ControlGUI.POWER_ON_COMMAND);
+    }
+
+    /**
+     * Handle updates to the state machine, display the current state in the GUI
+     */
+    @Override
+    public void update() {
+        ((JLabel)gui.getComponent("state")).setText(machine.getStateName());
+        ((JLabel)gui.getComponent("power")).setText(new Integer(((SuperCookingHood)machine).getPower()).toString());
+    }
+
+}
