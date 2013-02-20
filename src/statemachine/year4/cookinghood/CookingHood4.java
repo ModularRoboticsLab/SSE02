29ff06c58d061d56f22fb45ecfae6d6a24bf943e
Ulrik Pagh Schultz <ups@mmmi.sdu.dk>
2013-02-20 13:28:09 +0100

    Revert "Revert "Signal error rather than return  null""
    This reverts commit c725358613ee3746e84cfddce0fb851994894203.

--- src/statemachine/year4/cookinghood/CookingHood4.java
+++ src/statemachine/year4/cookinghood/CookingHood4.java
@@ -33,6 +33,7 @@ import javax.swing.JLabel;
 
 import statemachine.year1.library.GraphicalMachine;
 import statemachine.year1.cookinghood.CookingHood1.ControlGUI;
+import statemachine.year4.generated.CookingHood4Impl;
 
 /**
  * Cooking hood example using the code-generation CookingHood4Impl class
